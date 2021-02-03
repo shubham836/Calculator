f7a1f95b4b47d707bfea21277ab721def4e35c03
shubham <shubhamshukla836@gmail.com>
2021-02-03 13:51:54 +0530

    Fixed decimal bug

--- app/src/main/java/com/navideous/calculator/MainActivity.kt
+++ app/src/main/java/com/navideous/calculator/MainActivity.kt
@@ -2,14 +2,19 @@ package com.navideous.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.round
+import kotlin.math.roundToInt
+
 class MainActivity : AppCompatActivity() {
 
-    var lastNumeric:Boolean=false
-    var lastDot:Boolean=false
-    var calculating:Boolean=true
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+    var calculating: Boolean = true
+    var onClrPressed = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,32 +22,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onDigit(view:View){
-
+    fun onDigit(view: View) {
         if (calculating) {
             input.append((view as Button).text)
             lastNumeric = true
-        }else if (!calculating){
-            input.text=""
+            onClrPressed = false
+        } else if (!calculating) {
+            input.text = ""
             input.append((view as Button).text)
             lastNumeric = true
+            calculating = true
         }
     }
 
-    fun onClear(view: View){
-
-        input.text=""
-        lastNumeric=false
-        lastDot=false
+    fun onClear(view: View) {
+        onClrPressed = true
+        input.text = ""
+        lastNumeric = false
+        lastDot = false
     }
 
-    fun onDecimalPoint(view: View){
-
-        if (lastNumeric  && !lastDot){
-
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
             input.append(".")
-            lastNumeric=false
-            lastDot=true
+            lastNumeric = false
+            lastDot = true
+            calculating = true
         }
     }
 
@@ -51,7 +56,13 @@ class MainActivity : AppCompatActivity() {
             input.append((view as Button).text)
             lastNumeric = false
             lastDot = false
-            calculating=true
+            calculating = true
+            onClrPressed = false
+        }
+
+        if (onClrPressed && !lastNumeric && view.tag == "1") {
+            input.append((view as Button).text)
+            onClrPressed = false
         }
     }
 
@@ -113,30 +124,42 @@ class MainActivity : AppCompatActivity() {
 
                     input.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
                 }
-                calculating=false
+                calculating = false
+                lastDot = false
             } catch (e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
     }
 
-    fun isOperatorAdded(value:String):Boolean{
-        return if (value.startsWith("-")) {
-            false
-        } else {
-            (value.contains("/")
-                    || value.contains("*")
-                    || value.contains("-")
-                    || value.contains("+"))
+    private fun isOperatorAdded(value: String): Boolean {
+        val tempString = value.substring(1, value.length)
+        var tempBool = false
+
+        if (tempString.contains("/")
+            || tempString.contains("*")
+            || tempString.contains("-")
+            || tempString.contains("+")
+        ) {
+            tempBool = true
         }
+        return tempBool
     }
 
     private fun removeZeroAfterDot(result: String): String {
 
         var value = result
+        if (value.contains(".")) {
+            if (value.substring(value.indexOf(".") + 1, value.length).toLong() > 0) {
+                if (value.substring(value.indexOf(".") + 1, value.length).length > 3) {
+                    value = result.substring(0, result.indexOf(".") + 5)
 
-        if (result.contains(".0")) {
-            value = result.substring(0, result.length - 2)
+                } else {
+                    value = result
+                }
+            } else {
+                value = result.toDouble().roundToInt().toString()
+            }
         }
         return value
     }
